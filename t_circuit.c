f17099ee1be57f9701afb8ecf9adf1eb58e0a71a
JustinRoy992 <justin.roy.2@ens.etsmtl.ca>
2025-11-17 14:56:54 -0500

    d

--- t_circuit.c
+++ t_circuit.c
@@ -7,76 +7,99 @@ Auteur: Sébastien Hubert, Justin roy, Émile Larochelle
 
 #include "t_circuit.h"
 
-t_circuit* circuit_init(void)
+//justin
+t_circuit* circuit_init(void) 
 {
 
 }
 
+//emile
 void circuit_destroy(t_circuit* circuit)
 {
 
 }
 
+//sebas
 t_porte* circuit_ajouter_porte(t_circuit* circuit, e_types_portes le_type, int id, char* nom)
 {
 
 }
 
+//justin
 t_source* circuit_ajouter_source(t_circuit* circuit, int id, char* nom)
 {
 
 }
 
+//emile
 t_dest* circuit_ajouter_dest(t_circuit* circuit, int id, char* nom)
 {
 
 }
 
+
+//sebas
 int circuit_est_valide(t_circuit* circuit)
 {
 
 }
 
+
+//justin
 int circuit_appliquer_signal(t_circuit* circuit, int signal[], int nb_bits)
 {
 
 }
 
+
+//emile
 void circuit_reset(t_circuit* circuit)
 {
 
 }
 
+
+//sebas
 int circuit_propager_signal(t_circuit* circuit)
 {
 
 }
 
+
+//justin
 int circuit_get_nb_portes(const t_circuit* circuit)
 {
 
 }
 
+
+//emile
 t_porte* circuit_get_porte(const t_circuit* circuit, int pos)
 {
 
 }
 
+
+//sebas
 int circuit_get_nb_dest(const t_circuit* circuit)
 {
 
 }
 
+//justin
 t_dest* circuit_get_dest(const t_circuit* circuit, int pos)
 {
 
 }
 
+//emile
 int circuit_get_nb_sources(const t_circuit* circuit)
 {
 
 }
 
+
+//sebas
 t_source* circuit_get_source(const t_circuit* circuit, int pos)
 {
 
@@ -87,4 +110,4 @@ t_source* circuit_get_source(const t_circuit* circuit, int pos)
 //... _source()
 //... _destroy()
 // TEST:
->>>>>> > b8eb6a6
+//>>>>>> > b8eb6a6
