0d887cdb58528a2ed70fe67343c57f7da3af0a0a
EmileLarochelle <emilelarochelle1@gmail.com>
2025-11-17 15:49:26 -0500

    liste de fonction a faire par qui

--- t_source.c
+++ t_source.c
@@ -7,52 +7,52 @@ Auteur: Sébastien Hubert, Justin roy, Émile Larochelle
 */
 
 #include "t_source.h"
-
+// justin roy
 t_source* source_init(int id, char* nom)
 {
 
 }
-
+// émile larochelle
 void source_destroy(t_source* source)
 {
 
 }
-
+//Sébastien Hubert
 t_pin_sortie* source_get_pin(t_source* source)
 {
 
 }
-
+//justin roy
 int source_est_reliee(t_source* source)
 {
 
 }
-
+//émile larochelle
 void source_reset(t_source* source)
 {
 
 }
-
+//sébastien hubert
 int source_propager_signal(t_source* source)
 {
 
 }
-
+//justin roy
 int source_get_valeur(const t_source* source)
 {
 
 }
-
+//émile larochelle
 int source_get_id(const t_source* source)
 {
 
 }
-
+//sébastien hubert
 char* source_get_nom(const t_source* source)
 {
 
 }
-
+//justin roy
 void source_serialiser(const t_source* source, char* resultat)
 {
 
