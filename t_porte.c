0f35b5e6aa2e9b094fd8d2b9de3ded1433a7d8bd
JustinRoy992 <justin.roy.2@ens.etsmtl.ca>
2025-11-17 13:47:53 -0500

    création de t_porte.c

---
+++ t_porte.c
@@ -0,0 +1,9 @@
+//t_porte*porte_init(id,type,nom)
+//tporte*porte=...mallo (1 seul)
+//copier le 'Id' recu
+//allouer l'espace - mémoire pour le 'nom' et y copier le 'Nom' reçu
+//selon le 'type' reçu;
+ //créer les 2 (ou une) pin-entrée(s)
+//créer la pin-sortie (pin_sortie_init())
+//nb_entrée = 1 ou 2
+//retun porte;
