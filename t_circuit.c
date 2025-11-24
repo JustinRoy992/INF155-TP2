13a2e01aa461f428812a71b83ddffd528eac4205
Seb-Hubert <sebastien.hubert.3@ens.etsmtl.ca>
2025-11-24 16:48:21 -0500

    Tavail sur t_circuit.c, reste propager et apliquer et annoter

--- t_circuit.c
+++ t_circuit.c
@@ -7,103 +7,177 @@ Auteur: Sébastien Hubert, Justin roy, Émile Larochelle
 
 #include "t_circuit.h"
 
-//justin
+
 t_circuit* circuit_init(void) 
 {
+	t_circuit* nouveau_circuit;
 
+	nouveau_circuit = (t_circuit*)malloc(sizeof(t_circuit));
+
+	nouveau_circuit->nb_sources = 0;
+	nouveau_circuit->nb_destinations = 0;
+	nouveau_circuit->nb_portes = 0;
+
+	return nouveau_circuit;
 }
 
-//emile
+
 void circuit_destroy(t_circuit* circuit)
 {
+	source_destroy(circuit->sources);
+	dest_destroy(circuit->destinations);
+	porte_destroy(circuit->portes);
 
+	free(circuit);
 }
 
-//sebas
+
 t_porte* circuit_ajouter_porte(t_circuit* circuit, e_types_portes le_type, int id, char* nom)
 {
+	if (circuit->nb_portes < MAX_PORTES)
+	{
+		circuit->portes[circuit->nb_portes] = porte_init(id, le_type, nom);
+		circuit->nb_portes++;
 
+		return circuit->portes[circuit->nb_portes - 1];
+	}
+	return NULL;
 }
 
-//justin
+
 t_source* circuit_ajouter_source(t_circuit* circuit, int id, char* nom)
 {
+	if (circuit->nb_sources < MAX_SOURCES)
+	{
+		circuit->sources[circuit->nb_sources] = source_init(id, nom);
+		circuit->nb_sources++;
 
+		return circuit->sources[circuit->nb_sources - 1];
+	}
+	return NULL;
 }
 
-//emile
+
 t_dest* circuit_ajouter_dest(t_circuit* circuit, int id, char* nom)
 {
+	if (circuit->nb_destinations < MAX_DEST)
+	{
+		circuit->destinations[circuit->nb_destinations] = dest_init(id, nom);
+		circuit->nb_destinations++;
 
+		return circuit->destinations[circuit->nb_destinations - 1];
+	}
+	return NULL;
 }
 
 
-//sebas
 int circuit_est_valide(t_circuit* circuit)
 {
-
+	int i;
+	
+	for (i = 0; i < circuit->nb_portes; i++)
+	{
+		if (!porte_est_reliee(circuit->portes[i]))
+		{
+			return 0;
+		}
+	}
+	for (i = 0; i < circuit->nb_sources; i++)
+	{
+		if (!source_est_reliee(circuit->sources))
+		{
+			return 0;
+		}
+	}
+	for (i = 0; i < circuit->nb_destinations; i++)
+	{
+		if (!dest_est_reliee(circuit->destinations))
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
 
-//justin
 int circuit_appliquer_signal(t_circuit* circuit, int signal[], int nb_bits)
 {
-
+	
 }
 
 
-//emile
 void circuit_reset(t_circuit* circuit)
 {
+	int i;
 
+	for (i = 0; i < circuit->nb_portes; i++)
+	{
+		porte_reset(circuit->portes[i]);
+	}
+	for (i = 0; i < circuit->nb_sources; i++)
+	{
+		source_reset(circuit->sources[i]);
+	}
+	for (i = 0; i < circuit->nb_destinations; i++)
+	{
+		dest_reset(circuit->destinations[i]);
+	}
 }
 
 
-//sebas
 int circuit_propager_signal(t_circuit* circuit)
 {
 
 }
 
 
-//justin
 int circuit_get_nb_portes(const t_circuit* circuit)
 {
-
+	return circuit->nb_portes;
 }
 
 
-//emile
 t_porte* circuit_get_porte(const t_circuit* circuit, int pos)
 {
-
+	if (0 < pos && pos < circuit->nb_portes)
+	{
+		return circuit->portes[pos];
+	}
+	return NULL;
 }
 
 
-//sebas
 int circuit_get_nb_dest(const t_circuit* circuit)
 {
-
+	return circuit->nb_destinations;
 }
 
-//justin
+
 t_dest* circuit_get_dest(const t_circuit* circuit, int pos)
 {
-
+	if (0 < pos && pos < circuit->nb_destinations)
+	{
+		return circuit->destinations[pos];
+	}
+	return NULL;
 }
 
-//emile
+
 int circuit_get_nb_sources(const t_circuit* circuit)
 {
-
+	return circuit->nb_sources;
 }
 
 
-//sebas
 t_source* circuit_get_source(const t_circuit* circuit, int pos)
 {
-
+	if (0 < pos && pos < circuit->nb_sources)
+	{
+		return circuit->sources[pos];
+	}
+	return NULL;
 }
+
 // -circuit-intit()
 // -circuit_destroy() // 3 boucles
 // -circuit_ajouter_porte()
